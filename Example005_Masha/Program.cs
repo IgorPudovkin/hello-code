66da2beadc09026631a161d5a2873145033f1ec7
Igor <denlandpud@gmail.com>
2023-02-08 21:17:48 +0300

    Example005_Masha

---
+++ Example005_Masha/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите имя пользователяЖ ");
+string usename = Console.ReadLine();
+
+if(usename.ToLower()  == "masha")
+{
+    Console.WriteLine("Hello Beautiful Masha");
+}
+    
+
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(usename);
+}
+
+
